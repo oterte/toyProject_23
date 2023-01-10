b95d614e6915d22b6adca37cb7f20307268e7524
oterte <97093479+oterte@users.noreply.github.com>
2023-01-10 21:03:58 +0900

    테스트

--- javascript/main.js
+++ javascript/main.js
@@ -3,8 +3,33 @@ function register(){
 }
 
 
+let objUsers = [
+    {
+        username:"kim",
+        password:"1234"
+    },
+    {
+        username:"park",
+        password:"12345"
+    },
+    {
+        username:"lee",
+        password:"123456"
+    }
+]
+
 function login(){
-    window.location.href='fanclub.html';
-    alert('환영합니다 아무개님');
+    let userId = document.getElementById("userId").value
+    let userPW = document.getElementById("userPW").value
+    for(i = 0; i <objUsers.length; i++){
+        if(userId == objUsers[i].username && userPW == objUsers[i].password){
+            alert('환영합니다 ' + userId +'님');
+            window.location.href='register.html'
+        }
+    }alert("아이디나 비밀번호를 확인하세요") 
+    
+    
+        
 
 }
+

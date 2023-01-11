1ed4ae7fecc3a4ffadc47b92ab9ddf1d71026855
oterte <97093479+oterte@users.noreply.github.com>
2023-01-11 18:46:02 +0900

    댓글추가, 삭제

--- javascript/fanclub.js
+++ javascript/fanclub.js
@@ -1,24 +1,75 @@
+// 다른 함수에서 사용하고 싶은 변수들
+let name = $('#floatingInput').val() 
+let comment = $('#floatingTextarea').val()
+let commentcnt = 0;
+const list = 
+[{
+    name:'김명주',
+    comment:'새 앨범 기대합니다'
+    
+}]
+function init(){
+    // 디비에서 있는 리스트 불러오기
+    for(i = 0; i<list.length; i++){
+        let temp_html =
+         `<div class="card" id="cardBox-${i}">
+<div class="card-body">
+<blockquote class="blockquote mb-0">
+    <p>${list[i].comment}</p>
+    <footer class="blockquote-footer">${list[i].name} </footer>
+    <button type="button" class="btn btn-dark" onclick="delete_comment(${i})">삭제</button>
+</blockquote>
+
+</div>
+</div>`;
+    $('#cardArea').append(temp_html)
+    commentcnt++;
+    }
+}
+
+function updateInput(){
+    const name = $('#floatingInput').val() 
+    const comment = $('#floatingTextarea').val()
+    return {name,comment}
+}
+
+function fadeOut(){
+    const img = document.getElementById('img1')
+    img.style.display='none'
+}
+
+
+
+
+
 function add_comment() {
-    let name = $('#floatingInput').val()
-    let comment = $('#floatingTextarea').val()
-    let temp_html = `<div class="card">
+commentcnt = commentcnt+1;
+const {name, comment} = updateInput();
+list.push({name, comment})// 디비에 집어넣는 포인트
+console.log(list)
+    let temp_html = `<div class="card" id="cardBox-${commentcnt}">
 <div class="card-body">
 <blockquote class="blockquote mb-0">
     <p>${comment}</p>
     <footer class="blockquote-footer">${name} </footer>
-    <button type="button" class="btn btn-dark" onclick="delete_comment()">삭제</button>
+    <button type="button" class="btn btn-dark" onclick="delete_comment(${commentcnt})">삭제</button>
 </blockquote>
 
 </div>
 </div>`
-    $('#cardArea').append(temp_html)
-}
-
-
-
-function delete_comment(){
-    alert('삭제하시겠습니까?')
-    const div = document.getElementById('cardBox')
-    div.remove();
+    if(name == ""  || comment == ""){
+        alert("닉네임과 내용을 정확히 입력해주세요")
+    }else{
+        $('#cardArea').append(temp_html)
+    }
 
 }
+
+
+
+function delete_comment(cardNumber){
+    const delete_card = $(`#cardBox-${cardNumber}`)
+    delete_card.remove();
+}
+
+init();
